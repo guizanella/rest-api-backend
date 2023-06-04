ce0840bea9579f752cd1529b1d9eea31975855bc
Guilherme Restelatto Zanella <guizanella3@gmail.com>
2023-06-04 10:18:00 -0300

    endpoint candidatar

--- controllers/candidatoProjetoController.js
+++ controllers/candidatoProjetoController.js
@@ -1,12 +1,18 @@
 const db = require('../config/db')
+const msg = "Operação não autorizada."
 
 module.exports = {
 
-    async postCandidatoProjeto(req, res) {
-        const candidatoProjeto = new db.candidatoProjeto(req.body);
+    async postCandidatar(req, res) {
+        if (!req.session.Usuario) return res.status(401).json(msg)
 
-        await candidatoProjeto.save().then((candidatoProjeto) => {
-            return res.json({ "data": { "status": 201, candidatoProjeto } });
-        });
+        if (req.session.Usuario[0].tipo == 3) {
+            const candidatoProjeto = new db.candidatoProjeto(req.body);
+            candidatoProjeto.usuarioId = req.session.Usuario[0].id
+
+            await candidatoProjeto.save().then((candidatoProjeto) => {
+                return res.status(201).json({ "data": { candidatoProjeto } });
+            });
+        } else return res.status(401).json(msg)
     }
 }
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const route = express.Router()
 const usuarioController = require('../controllers/usuarioController')
-const projetoController = require('../controllers/projetoController')
-//const controllerSession = require('../controller/controllerSession')
+const projetoController = require('../controllers/projetoController');
+const candidatoProjetoController = require('../controllers/candidatoProjetoController');
 
 // Autenticação
 route.post("/usuario/login", usuarioController.postLogin);
@@ -20,28 +20,7 @@ route.post("/projeto", projetoController.postProjeto);
 route.put('/projeto/:id', projetoController.putProjeto);
 route.delete('/projeto/:id', projetoController.deleteProjeto);
 
-
-/*
-route.get('/', controllerSession.getInicio)
-route.get('/logout', controllerSession.getLogout)
-route.post('/login', controllerSession.postLogin)
-route.post('/logout', controllerSession.postLogout)
-
-route.get('/createUser', controllerUser.getCreate)
-route.post('/createUser', controllerUser.postCreate)
-route.get('/usersList', controllerUser.getList)
-route.get("/editUser/:id", controllerUser.getEdit);
-route.post("/editUser", controllerUser.postEdit);
-route.get("/deleteUser/:id", controllerUser.getDelete);
-
-route.get('/createPresentation', controllerPresentation.getCreate)
-route.post('/createPresentation', controllerPresentation.postCreate)
-route.get('/presentationList', controllerPresentation.getList)
-route.get('/editPresentation/:id', controllerPresentation.getEdit)
-route.post('/editPresentation', controllerPresentation.postEdit)
-route.get('/deletePresentation/:id', controllerPresentation.getDelete)
-route.get('/abreVotacao', controllerPresentation.postAbreFechaVotacao)
-route.get('/votacao', controllerPresentation.getListVotacao)
-route.post('/votacao', controllerPresentation.postVotacao)*/
+// Candidatar
+route.post("/candidatar", candidatoProjetoController.postCandidatar);
 
 module.exports = route;
